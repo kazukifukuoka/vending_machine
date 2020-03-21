896ef824c315a3658d0b633e92fa06f1970fb1c9
福岡和樹 <kazuki@mba.local>
2020-03-21 12:36:18 +0900

    refund_money

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,5 +1,4 @@
 class VendingMachine
-  AVAILABLE_MONEY = [10, 50, 100, 500, 1000].freeze
 
   def initialize(machine:)
     @machine = machine
@@ -7,59 +6,4 @@ class VendingMachine
     @drink_table = []
   end
 
-  def insert
-    puts <<~text
-
-      お金を何円入れますか？
-      10,50,100,500,1000から選んで入力してください
-
-    text
-    @money = gets.to_i
-    if AVAILABLE_MONEY.include?(@money)
-      @total += @money
-      puts "自動販売機には#{@total}円入っています"
-    else
-      puts <<~text
-
-        指定された金額から選んでください
-      text
-      refund
-    end
-  end
-
-
-  def choices
-    puts <<~text
-
-      次はどうする？
-      1. 飲み物を選ぶ
-      2. 追加でお金を入れる
-      3. 投入金額を確認する
-      4. お釣りを出す
-
-    text
-  end
-
-  def refund
-    if AVAILABLE_MONEY.include?(@money)
-      refund_money = @total
-    else
-      refund_money = @money
-    end
-    puts <<~text
-
-      #{refund_money}円が戻ってきた
-
-    text
-    refund_money = @total
-  end
-
-  def confirm
-
-    puts <<~text
-
-    自動販売機には#{@total}円入っています
-
-    text
-  end
 end
