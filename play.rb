9e3eae989b9e803fcdaa222a702a2b8288c30b6a
福岡和樹 <kazuki@mba.local>
2020-03-22 13:22:06 +0900

    buy_juice

--- play.rb
+++ play.rb
@@ -1,24 +1,11 @@
 require './user'
-require './vending_machine'
-require './drink'
 
 user = User.new
-
 # プログラムの実行
 loop do
   user.action
   case  gets.to_i
   when 1
-    puts <<~text
-
-    何を購入しますか？
-    1. 水(100円)
-    2. コーラ(150円)
-    3. モンスター(210円)
-
-    text
-    Drink.buy
-    Drink.buy_message
     user.buy
   when 2
     user.insert
@@ -27,5 +14,7 @@ loop do
   when 4
     user.refund
     break
+  else
+    puts "1~4の番号を入力して下さい"
   end
 end
--- user.rb
+++ user.rb
@@ -1,8 +1,10 @@
+require './drink'
+
 class User
   # ユーザーが使えるお金
   AVAILABLE_MONEY = [10, 50, 100, 500, 1000].freeze
 
-  def initialize
+  def initialize(**params)
     @total_insert_money = 0
     @refund_money = 0
   end
@@ -15,26 +17,55 @@ class User
       1. 飲み物を選ぶ
       2. 追加でお金を入れる
       3. 投入金額を確認する
-      4. お釣りを出す
+      4. お釣りを出して終了
 
     text
   end
 
   # 1. 飲み物の購入
   def buy
-    # puts <<~text
-
-    # 何を購入しますか？
-    # 1. 水(100円)
-    # 2. コーラ(150円)
-    # 3. モンスター(210円)
-
-    # text
-    # Drink.buy
-    # Drink.buy_message
     water = Drink.new(name: "水", price: 100, stock: 10)
-    @juice = water
-    puts "#{@juice.price}"
+    cola = Drink.new(name: "コーラ", price: 150, stock: 5)
+    monster = Drink.new(name: "モンスター", price: 210, stock: 3)
+    puts <<~text
+
+    何を購入しますか？
+    1. 水(100円)
+    2. コーラ(150円)
+    3. モンスター(210円)
+
+    text
+
+    case  gets.to_i
+    when 1
+      juice = water
+    when 2
+      juice = cola
+    when 3
+      juice = monster
+    end
+
+    if @total_insert_money >= juice.price
+      puts <<~text
+
+      #{juice.price}円の#{juice.name}を購入しました
+
+    text
+    @total_insert_money -= juice.price
+      if @total_insert_money > 0
+        puts <<~text
+
+          お買い上げありがとうございます
+          自動販売機には#{@total_insert_money}円が入っています
+        text
+      elsif @total_insert_money == 0
+        puts "お買い上げありがとうございます"
+      end
+    @refund_money = @total_insert_money
+    else
+      puts "お金が足りません"
+    end
+
   end
 
   # 2. お金を入れる
@@ -82,8 +113,13 @@ class User
       text
       @refund_money = @total_insert_money
     else
-      puts "お金を入れてください"
+      puts <<~text
+
+        お釣りはありません
+        ありがとうございました
+
+      text
+
     end
   end
-
 end
