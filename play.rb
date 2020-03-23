6f62b66e1da03643ca7e2a2ba644a3a0a0c190fa
福岡和樹 <kazuki@mba.local>
2020-03-23 17:14:40 +0900

    vender_machine

--- play.rb
+++ play.rb
@@ -3,7 +3,6 @@ require './message_to_user'
 
 class Play
   include MessageToUser
-  AVAILABLE_MONEY = [10, 50, 100, 500, 1000].freeze
   vender = VenderMachine.new
 # プログラムの実行
   loop do
@@ -13,11 +12,11 @@ class Play
     when 1
       MessageToUser.select_juice_message
       select_juice_num = gets.to_i
-      judge_to_buy = vender.buy(select_juice_num)
-      if judge_to_buy == true
+      buy_flag = vender.buy(select_juice_num)
+      if buy_flag == true
         MessageToUser.buy_juice_message(vender.juice[select_juice_num])
         MessageToUser.enough_money_message(vender.total_insert_money)
-      elsif judge_to_buy == false
+      elsif buy_flag == false
         MessageToUser.error_message
       else
         MessageToUser.not_enough_money_message
@@ -25,8 +24,8 @@ class Play
     when 2
       MessageToUser.insert_money_message
       insert_money = gets.to_i
-      vender.insert(insert_money)
-      if AVAILABLE_MONEY.include?(insert_money)
+      insert_flag = vender.insert(insert_money)
+      if insert_flag
         MessageToUser.confirm_money_message(vender.total_insert_money)
       else
         MessageToUser.error_message
